147bedd2318d700236cd5c16f8c2052805ee2e82
SamRong <YungSun3307@163.com>
2021-02-06 23:54:13 +0800

    修复import报错

--- Canvas.js
+++ Canvas.js
@@ -52,4 +52,25 @@ function drawline(x1, y1, x2, y2) {
     ctx.stroke();
 }
 
-export default ctx
+
+let red = document.querySelector('.red')
+let yellow = document.querySelector('.yellow')
+let blue = document.querySelector('.blue')
+let green = document.querySelector('.green')
+let black = document.querySelector('.black')
+let colorList = [red, yellow, blue, green, black]
+
+const colorCode = {
+    red: '#FF0000',
+    yellow: '#FFFF00',
+    blue: '#0000FF',
+    green: '#00FF00',
+    black: '#000000'
+}
+
+colorList.forEach(item => {
+    item.addEventListener('click', () => {
+        console.log(item)
+        ctx.strokeStyle = colorCode[item.className]
+    })
+})
